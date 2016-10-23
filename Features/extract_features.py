55e94f949c073a6fa06d276c3a424e7f8fd55ad1
Blomakrans <simonb92@gmail.com>
2016-10-23 21:50:33 +0200

    Baetti vid feature. 	# Please enter the commit message for your changes. Lines starting

--- Features/extract_features.py
+++ Features/extract_features.py
@@ -121,6 +121,131 @@ def flattenedImages(imgDirFullPath):
 	print "Done"
 	return images
 
+# Extract no. of voxels w. intensity above 1000, 1200 and 1400 in each of 4 segments.
+# The segments are front-upper, back-upper, front-lower and back-lower.
+# The data is pickled into a list of patients which for each patient contains
+# a length 4 list with the data for each brain segment.
+def extractWhiteMatter(imgDirFullPath):
+	imgPath = os.path.join(imgDirFullPath,"*")
+	print "imgPath = "+imgPath+""
+
+	outputFileName = os.path.join(featuresDir,"whitematter_"+imgDirFullPath.replace(os.sep,"-")+".feature")
+	if os.path.isfile(outputFileName):
+		save = open(outputFileName,'rb')
+		whitematter = pickle.load(save)
+		save.close()
+		return averages
+
+	# Fetch all directory listings of set_train
+	allImageSrc = sorted(glob.glob(imgPath), key=extractImgNumber)
+
+	whitematter = [] #averages = []
+	n_samples = len(allImageSrc);
+	print "Found "+str(n_samples)+" images!"
+	print "Preparing the data"
+
+	thresh = 1000;
+	for i in range(0,n_samples):
+		img = nib.load(allImageSrc[i])
+		imgData = img.get_data() #.flatten().tolist();
+		[xlim, ylim, zlim, Ilim] = imgData.shape
+		[x_border, z_border] = [int(xlim/2), int(zlim/2)] 
+		brainregions = [];
+		# back-lower
+		count_1000 = 0
+		count_1200 = 0
+		count_1400 = 0
+		for x in range(0,x_border):
+			for y in range(0,ylim-1):
+				for z in range(0,z_border):
+					voxel = imgData[x,y,z,0]
+					if( voxel > 1000 ): 
+						if( voxel > 1200 ):
+							if( voxel > 1400 ):  
+								count_1400 += 1
+							else:
+								count_1200 += 1
+						else:
+							count_1000 += 1
+
+		brainregions.append(count_1000);
+		brainregions.append(count_1200);
+		brainregions.append(count_1400);
+		# front-lower
+		count_1000 = 0
+		count_1200 = 0
+		count_1400 = 0
+		for x in range(x_border,xlim-1):
+			for y in range(0,ylim-1):
+				for z in range(0,z_border):
+					voxel = imgData[x,y,z,0]
+					if( voxel > 1000 ): 
+						if( voxel > 1200 ):
+							if( voxel > 1400 ):  
+								count_1400 += 1
+							else:
+								count_1200 += 1
+						else:
+							count_1000 += 1
+
+		brainregions.append(count_1000);
+		brainregions.append(count_1200);
+		brainregions.append(count_1400);
+		# back upper
+		count_1000 = 0
+		count_1200 = 0
+		count_1400 = 0
+		for x in range(0,x_border):
+			for y in range(0,ylim-1):
+				for z in range(z_border,zlim-1):
+					voxel = imgData[x,y,z,0]
+					if( voxel > 1000 ): 
+						if( voxel > 1200 ):
+							if( voxel > 1400 ):  
+								count_1400 += 1
+							else:
+								count_1200 += 1
+						else:
+							count_1000 += 1
+
+		brainregions.append(count_1000);
+		brainregions.append(count_1200);
+		brainregions.append(count_1400);
+		# front upper
+		count_1000 = 0
+		count_1200 = 0
+		count_1400 = 0
+		for x in range(x_border,xlim-1):
+			for y in range(0,ylim-1):
+				for z in range(z_border,zlim-1):
+					voxel = imgData[x,y,z,0]
+					if( voxel > 1000 ): 
+						if( voxel > 1200 ):
+							if( voxel > 1400 ):  
+								count_1400 += 1
+							else:
+								count_1200 += 1
+						else:
+							count_1000 += 1
+
+		brainregions.append(count_1000);
+		brainregions.append(count_1200);
+		brainregions.append(count_1400);
+
+		whitematter.append(brainregions)
+		printProgress(i, n_samples)
+
+	printProgress(n_samples, n_samples)
+	print "Done"
+	print "\nStoring the features in "+outputFileName
+
+	output = open(outputFileName,"wb")
+	pickle.dump(whitematter,output)
+	output.close()
+	print "Done"
+	return whitematter
+
+
 def extractImgNumber(imgPath):
 	imgName = imgPath.split(os.sep)[-1]
 	imgNum = int(imgName.split('_')[-1][:-4])
